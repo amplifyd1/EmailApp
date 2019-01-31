caf2fb8f57814e63d20b4242b7d6375660e0c208
wesley sprague <wesleysprague@yahoo.com>
2019-01-31 10:21:20 -0500

    Generated array of students based on user criteria

--- javaprojects/src/studentdatabaseapp/studentDatabaseApp.java
+++ javaprojects/src/studentdatabaseapp/studentDatabaseApp.java
@@ -1,13 +1,27 @@
 package studentdatabaseapp;
+import java.util.Scanner;
 
 public class studentDatabaseApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Student std1 = new Student();
-		std1.enroll();
-		std1.payTuition();
-		System.out.println(std1.showInfo());
+	
+		//Ask for number of students 
+		System.out.println("Enter number of new students to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
+		
+		//Create Students 
+		for(int n = 0; n < numOfStudents; n++) {
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].payTuition();
+		}
+		
+		for(int n = 0; n < numOfStudents; n++) {
+			System.out.println(students[n].showInfo());
+		}
+		
 	}
 
 }
